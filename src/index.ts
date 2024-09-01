c4b4af3df0158750ff00ae6307b7786e0a6ca1f5
shariq-yousuf <shariqyousuf01@gmail.com>
2024-09-01 10:30:35 +0500

    create the vite with ts by my own then setupt with the boiler code provided by scrimba and add auth

---
+++ src/index.ts
@@ -0,0 +1,106 @@
+/* === Imports === */
+import "./index.css"
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth"
+
+/* === Firebase Setup === */
+const firebaseConfig = {
+  apiKey: "AIzaSyCMk0RrBozlUEbDlcVlfGqfR1MJLb6u7j8",
+  authDomain: "moody-7df10.firebaseapp.com",
+  projectId: "moody-7df10",
+  storageBucket: "moody-7df10.appspot.com",
+  appId: "1:251233526924:web:fe4da7397615bc3c8a47a9",
+}
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const auth = getAuth(app)
+
+/* === UI === */
+
+/* == UI - Elements == */
+
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
+
+const signInWithGoogleButtonEl = document.getElementById(
+  "sign-in-with-google-btn"
+)
+
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
+
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("create-account-btn")
+
+/* == UI - Event Listeners == */
+
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
+
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+
+/* === Main Code === */
+
+showLoggedOutView()
+
+/* === Functions === */
+
+/* = Functions - Firebase - Authentication = */
+
+function authSignInWithGoogle() {
+  console.log("Sign in with Google")
+}
+
+function authSignInWithEmail() {
+  console.log("Sign in with email and password")
+}
+
+async function authCreateAccountWithEmail() {
+  const email = (emailInputEl as HTMLFormElement).value
+  const password = (passwordInputEl as HTMLFormElement).value
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    )
+    sendEmailVerification(auth.currentUser)
+    showLoggedInView()
+    console.log(auth)
+  } catch (error) {
+    console.error("Sign out failed:", getErrorMessage(error))
+  }
+}
+
+/* == Functions - UI Functions == */
+
+function showLoggedOutView() {
+  hideElement(viewLoggedIn)
+  showElement(viewLoggedOut)
+}
+
+function showLoggedInView() {
+  hideElement(viewLoggedOut)
+  showElement(viewLoggedIn)
+}
+
+function showElement(element: HTMLElement) {
+  element.style.display = "flex"
+}
+
+function hideElement(element: HTMLElement) {
+  element.style.display = "none"
+}
+
+/* == Utils == */
+function getErrorMessage(error: unknown): string {
+  if (error instanceof Error) return error.message
+  return String(error)
+}
